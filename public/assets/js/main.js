1f1d84448a4af0b32648f9a18717cfe65a371157
dikaproject <dikagilang2007@gmail.com>
2024-06-28 10:43:13 +0700

    version1.0

---
+++ public/assets/js/main.js
@@ -0,0 +1,57 @@
+
+AOS.init({
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 900, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
+
+
+function countdownTimer() {
+  const countDownDate = new Date("2024-07-10").getTime();
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("days").innerText = "0";
+    document.getElementById("hours").innerText = "0";
+    document.getElementById("minutes").innerText = "0";
+    document.getElementById("seconds").innerText = "0";
+  }
+}
+
+const x = setInterval(countdownTimer, 1000);
+
+
+const faqs = document.querySelectorAll(".faq")
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    if (faq.classList.contains("active")) {
+      faq.classList.remove("active")
+    } else {
+      faq.classList.add("active")
+      faqs.forEach((otherFaq) => {
+        if (otherFaq != faq) {
+          otherFaq.classList.remove("active")
+        }
+      })
+    }
+  })
+})
